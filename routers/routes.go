a0705c68e8b174dd626666b3508302d8e8055519
HRH <1165276516@qq.com>
2021-12-26 01:14:25 +0800

    update routers

--- routers/routes.go
+++ routers/routes.go
@@ -7,8 +7,8 @@ import (
 )
 
 func CollectRoute(r *gin.Engine) *gin.Engine {
-	r.POST("/api/auth/register", controller.Register)
-	r.POST("/api/auth/login", controller.Login)
-	r.GET("/api/auth/info", middleware.AuthMiddleware(), controller.Info)
+	r.POST("/user/register", controller.Register)
+	r.POST("/user/login", controller.Login)
+	r.GET("/user/me", middleware.AuthMiddleware(), controller.Info)
 	return r
 }
